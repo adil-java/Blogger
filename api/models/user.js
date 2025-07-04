8053e1b1312e991be478c735de1077a78350ce55
adil-java <adiljavaid125@gmail.com>
2025-07-04 11:36:38 -0700

    Restructuring the backend server files

--- api/models/user.js
+++
@@ -1,7 +0,0 @@
-// import * as yup from "yup";
-
-// export const UserSchema = yup.object({
-//   username: yup.string().min(3).required(),
-//   email: yup.string().email().required(),
-//   createdAt: yup.date().default(() => new Date()),
-// });
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express, { json } from "express"
+import "dotenv/config"
+import cors from "cors"
+import connectDB from "./configs/db.js";
+const app =express();
+
+await connectDB()
+// Middleware
+app.use(cors())
+app.use(express.json())
+//
+const port  = process.env.PORT || 3000;
+
+//Routes
+app.get("/",(req,res)=>{
+    res.send("Welcome to the server").status(200)
+})
+app.listen(port,()=>{
+    console.log("https://localhost:"+port)
+}
+)
+export default app;
